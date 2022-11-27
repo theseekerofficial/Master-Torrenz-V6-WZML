442512109ab66b44e5c437e8e93a0af5a95122c7
The Seeker <94602611+theseekerofficial@users.noreply.github.com>
2022-11-27 21:50:01 +0530

    Update wserver.py

--- web/wserver.py
+++ web/wserver.py
@@ -1,5 +1,9 @@
 from logging import getLogger, FileHandler, StreamHandler, INFO, basicConfig
 from time import sleep
+from time import sleep, time
+from psutil import boot_time, disk_usage, net_io_counters
+from subprocess import check_output
+from os import path as ospath
 from qbittorrentapi import NotFound404Error, Client as qbClient
 from aria2p import API as ariaAPI, Client as ariaClient
 from flask import Flask, request
@@ -772,6 +776,29 @@ def set_priority(id_):
             LOGGER.info(f"Verification Failed! Report! Gid: {id_}")
     return list_torrent_contents(id_)
 
+botStartTime = time()
+if ospath.exists('.git'):
+    commit_date = check_output(["git log -1 --date=format:'%y/%m/%d %H:%M' --pretty=format:'%cd'"], shell=True).decode()
+else:
+    commit_date = 'No UPSTREAM_REPO'
+
+@app.route('/status', methods=['GET'])
+def status():
+    bot_uptime = time() - botStartTime
+    uptime = time() - boot_time()
+    sent = net_io_counters().bytes_sent
+    recv = net_io_counters().bytes_recv
+    return {
+        'commit_date': commit_date,
+        'uptime': uptime,
+        'on_time': bot_uptime,
+        'free_disk': disk_usage('.').free,
+        'total_disk': disk_usage('.').total,
+        'network': {
+            'sent': sent,
+            'recv': recv,
+        },
+    }
 @app.route('/')
 def homepage():
     return "<h1>See mirror-with-seeker <a href='https://t.me/the_seeker_s_cave'>@GitHub</a> By <a href='https://t.me/the_seeker_s_cave'>Code With Seeker</a></h1>"
